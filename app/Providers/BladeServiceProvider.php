a258d625c30832433d6bde1a05ff84f6c36f0740
thuantong <tongminhthuan0405@gmail.com>
2021-03-04 04:21:00 +0700

    update service

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -30,16 +30,16 @@ class BladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $diaDiem = DiaDiem::all()->toArray();
-        // View::share('dia_diem', $diaDiem);
-        // $nganhNghe = NganhNghe::all()->toArray();
-        // View::share('nganh_nghe', $nganhNghe);
-        // $bangCap = BangCap::all()->toArray();
-        // View::share('bang_cap', $bangCap);
-        // $chucVu = ChucVu::all()->toArray();
-        // View::share('chuc_vu', $chucVu);
-        // $kieuLamViec = KieuLamViec::all()->toArray();
-        // View::share('kieu_lam_viec', $kieuLamViec);
+        $diaDiem = DiaDiem::all()->toArray();
+        View::share('dia_diem', $diaDiem);
+        $nganhNghe = NganhNghe::all()->toArray();
+        View::share('nganh_nghe', $nganhNghe);
+        $bangCap = BangCap::all()->toArray();
+        View::share('bang_cap', $bangCap);
+        $chucVu = ChucVu::all()->toArray();
+        View::share('chuc_vu', $chucVu);
+        $kieuLamViec = KieuLamViec::all()->toArray();
+        View::share('kieu_lam_viec', $kieuLamViec);
         Blade::directive('money_xu', function ($money) {
             return "<?php echo number_format($money, 0); ?>";
         });
