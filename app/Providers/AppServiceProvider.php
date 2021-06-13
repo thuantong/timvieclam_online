6679201370a5ca83311a82bac541b6ad176e478e
thuantong <tongminhthuan0405@gmail.com>
2021-06-14 01:54:46 +0700

    thuantong
    -c

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,10 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // if(env('REDIRECT_HTTPS'))
-        // {
-        //     URL::forceScheme('https');
-        // }
+        if(env('REDIRECT_HTTPS'))
+        {
+            URL::forceScheme('https');
+        }
 
         Schema::defaultStringLength(191);
 //        date_default_timezone_set('UTC');
